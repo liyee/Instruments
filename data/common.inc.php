d83838c00adabecf5396c1bf1f06f7225816d527
lijunqian <lijq@mutantbox.com>
2019-09-16 11:57:00 +0800

    本地化

--- data/common.inc.php
+++ data/common.inc.php
@@ -4,22 +4,21 @@
 
 $cfg_dbtype = 'mysql';
 
-$cfg_dbhost = 'localhost';
+// $cfg_dbhost = 'localhost';
+// $cfg_dbuser = 'tarzas9e_26789';
+// $cfg_dbpwd = 'qt7240348';
+
+// $cfg_dbhost = 'localhost';
+// $cfg_dbuser = 'root';
+// $cfg_dbpwd = '';
+
+$cfg_dbhost = '10.8.189.203';
+$cfg_dbuser = 'dbmanger';
+$cfg_dbpwd = 'edJelrZuAgpNeLyn';
 
 $cfg_dbname = 'tarzas9e_ta';
-
-// $cfg_dbuser = 'tarzas9e_26789';
-$cfg_dbuser = 'root';
-
-//$cfg_dbpwd = 'qt7240348';
-$cfg_dbpwd = '';
 
 $cfg_dbprefix = 'dede_';
 
 $cfg_db_language = 'utf8';
-
-
-
-
-
 ?>
